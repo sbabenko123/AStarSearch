a6b7951e6f22b5ad72c29d76481941daec0fb94f
Sofiya Babenko <106487965+sbabenko123@users.noreply.github.com>
2023-09-26 14:43:11 -0400

    Update AStarSearch.java

--- AStarSearch.java
+++ AStarSearch.java
@@ -1,47 +1,53 @@
 import java.util.*;
 
 public class AStarSearch {
-    public static void aStarSearch(BoardState current) {
-        if (current.wrong.size() <=1) {
-        	return;
-        }
-       
-        List<BoardState> neighbors = current.generateNeighbors();
-        float min = Integer.MAX_VALUE;
-        BoardState next = null;
-        for (BoardState neighbor : neighbors) {
-            if (neighbor.getFValue() < min) { //FIFO
-            	next = neighbor;
-            	min = neighbor.getFValue();
-            }
-        }
-        current.child = next;
-        aStarSearch(next);
-        return;
-    }
 
-public static void main(String[] args) {
-        // Define your initial 3x3 board state here.
-        int[][] initialBoard = {
-            {0, 1, 3},
-            {8, 2, 6},
-            {7, 4, 5}
-        };
+	/**
+	 * @param current The result of this method is the creation and assignment of
+	 *                child nodes that form the minimized path to the goal state.
+	 */
+	public static void aStarSearch(BoardState current) {
 
-        BoardState initialState = new BoardState(initialBoard);
-        aStarSearch(initialState);
+		// Base case
+		if (current.wrong.size() <= 1) {
+			return;
+		}
 
-        System.out.println("TRACE RESULTS=========================================");
-        result(initialState);
-    }
+		// Subproblem
+		List<BoardState> searchSpace = current.generateSearchSpace();
+		float min = Integer.MAX_VALUE;
+		BoardState next = null;
+		for (BoardState candidate : searchSpace) {
+			if (candidate.getFValue() < min) { // FIFO
+				next = candidate;
+				min = candidate.getFValue();
+			}
+		}
+
+		// Solution
+		current.child = next;
+		aStarSearch(next);
+		return;
+	}
+
+	public static void main(String[] args) {
+		int[][] initialBoard = { { 0, 1, 3 }, 
+					 { 8, 2, 6 }, 
+					 { 7, 4, 5 } };
+
+		BoardState initialState = new BoardState(initialBoard);
+		aStarSearch(initialState);
+
+		System.out.println("TRACE RESULTS=========================");
+		result(initialState);
+	}
 
 	public static void result(BoardState board) {
 		board.toString();
 		int i = 0;
-		while(board.child != null) {
+		while (board.child != null) {
 			board.child.toString();
 			board = board.child;
 		}
 	}
-
 }
